254a6ad058edb48006590ed1735a6443900dc025
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-29 09:59:54 +0600

    Add files via upload

---
+++ Onclick property.js
@@ -0,0 +1,10 @@
+var myVar =  document.querySelector("#myImgId");
+
+function myPicture1(){
+  
+    myVar.src = "Images/kaocher.jpg";
+}
+function myPicture2(){
+    
+    myVar.src = "Images/khalekuzzaman.jpg";
+  }
